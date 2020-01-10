99afe6bbbb4c0096ec8cc746e6fb59b9211b7e38
julian <house.of.jules@gmail.com>
2020-01-10 17:20:15 +1100

    Initial commit of UWGeo moresi_et_al '14
    * Mix of old material layouts into UWGeo material initialisation
    * Relative densities used with `relrho_geo_material_properties.py` as a
    recalibration of `geo_material_properties.py`
    * 2D no longer supported
    * Requires development branch of UWGeo. Fix for 3D tracers.

---
+++ geo_material_properties.py
@@ -0,0 +1,154 @@
+# core UW bit
+from UWGeodynamics.scaling import units as u
+
+import geo_model_properties as modprop
+
+alpha   = 3.0e-5 / u.degK
+
+# Density is defined using the dimensionless relative density
+mantle_density   = 3400 * u.kilogram / u.metre**3 # at surface
+ref_density      = mantle_density * (modprop.Tint-modprop.Tsurf) * alpha
+
+# Material properties
+um = {
+    'name'     : 'upper mantle',
+    'index'    : 0,
+    'viscosity': 1. * u.pascal * u.second * 1e20, 
+    'density'  : ref_density,
+    'cohesion' : 1e3 * u.megapascal,
+    'cohesion2': 1e3 * u.megapascal,
+}
+
+lm = {
+    'name'     : 'lower mantle',
+    'index'    : 1,
+    'viscosity': 100. * u.pascal * u.second * 1e20, 
+    'density'  : ref_density,
+    'cohesion' : 1e3 * u.megapascal,
+    'cohesion2': 1e3 * u.megapascal,
+}
+
+
+# 80Ma oceanic lithosphere
+subplate1 = {
+    'name'     : 'oceanic plate 1',
+    'index'    : 3,
+    'viscosity':  1.000e+05 * u.pascal * u.second * 1e20, 
+    'density'  : -3.388e-01 * ref_density + ref_density,
+    'cohesion' :  1.250e+01 * u.megapascal,
+    'cohesion2':  6.250e+00 * u.megapascal,        
+}
+
+subplate1_phase = {
+    'name'     : 'oceanic plate 1 after phase change',
+    'index'    : 2,
+    'viscosity':  1.000e+05 * u.pascal * u.second * 1e20, 
+    'density'  :  1.107e+00 * ref_density + ref_density,
+    'cohesion' :  1.250e+01 * u.megapascal,
+    'cohesion2':  6.250e+00 * u.megapascal,        
+}
+
+subplate2 = {
+    'name'     : 'oceanic plate 2',
+    'index'    : 4,
+    'viscosity':  1.000e+05 * u.pascal * u.second * 1e20, 
+    'density'  :  6.040e-01 * ref_density + ref_density,
+    'cohesion' :  6.744e+01  * u.megapascal,
+    'cohesion2':  3.372e+01 * u.megapascal,      
+}
+
+          
+subplate3 = {
+    'name'     : 'oceanic plate 3',
+    'index'    : 5,
+    'viscosity': 1.930e+04 * u.pascal * u.second * 1e20, 
+    'density'  : 3.849e-01 * ref_density + ref_density,
+    'cohesion' : 1.213e+02 * u.megapascal,
+    'cohesion2': 6.066e+01 * u.megapascal,      
+}
+
+subplate4 = {
+    'name'     : 'oceanic plate 4',
+    'index'    : 6,
+    'viscosity': 9.641e+01 * u.pascal * u.second * 1e20, 
+    'density'  : 2.228e-01 * ref_density + ref_density,
+    'cohesion' : 1.000e+03 * u.megapascal,
+    'cohesion2': 1.000e+03 * u.megapascal,      
+}
+
+# weak back arc material properties
+backArc1 = {
+    'name'     : 'backArc1',
+    'index'    : 7,
+    'viscosity':  4.978e+03 * u.pascal * u.second * 1e20,   
+    'density'  : -1.198e+00 * ref_density + ref_density,
+    'cohesion' :  1.250e+01 * u.megapascal,   
+    'cohesion2':  6.250e+00 * u.megapascal,
+}
+backArc2 = {
+    'name'     : 'backArc2',
+    'index'    : 8,
+    'viscosity': 1.726e+02 * u.pascal * u.second * 1e20, 
+    'density'  : 1.162e-01 * ref_density + ref_density,
+    'cohesion' : 2.500e+01 * u.megapascal,   
+    'cohesion2': 1.250e+01 * u.megapascal,
+}
+
+# transitional lithosphere
+trans1 = {
+    'name'     : 'trans1',
+    'index'    : 9,
+    'viscosity':  5.000e+03 * u.pascal * u.second * 1e20, 
+    'density'  : -1.977e+00 * ref_density + ref_density,
+    'cohesion' :  1.000e+03 * u.megapascal,
+}
+trans2 = {
+    'name'     : 'trans2',
+    'index'    : 10,
+    'viscosity':  1.502e+03 * u.pascal * u.second * 1e20, 
+    'density'  :  2.550e-01 * ref_density + ref_density,
+    'cohesion' :  1.000e+03 * u.megapascal,
+}
+
+# cratonic lithosphere
+craton1 = {
+    'name'     : 'craton1',
+    'index'    : 11,
+    'viscosity':  5.000e+03 * u.pascal * u.second * 1e20, 
+    'density'  : -2.118e+00 * ref_density + ref_density,
+    'cohesion' :  1.000e+03 * u.megapascal,
+}
+
+craton2 = {
+    'name'     : 'craton2',
+    'index'    : 12,
+    'viscosity':  1.675e+03 * u.pascal * u.second * 1e20, 
+    'density'  :  2.533e-01 * ref_density + ref_density,
+    'cohesion' :  1.000e+03 * u.megapascal,
+}
+
+# assume ribbon and buoyant strip have cratonic material properties
+ribbon = {
+    'name'     : 'ribbon',
+    'index'    : 13,
+    'viscosity':  1e5 * u.pascal * u.second * 1e20, 
+    'density'  : -2.11* ref_density + ref_density,
+    'cohesion' : 1e3 * u.megapascal, 
+}
+
+buoyStrip = {
+    'name'     : 'buoyStrip',
+    'index'    : 14,
+    'viscosity':  1e5 * u.pascal * u.second * 1e20,    # strong 
+    'density'  : -2.11* ref_density + ref_density,   # assume cratonic density
+    'cohesion' : 1e3 * u.megapascal, 
+}
+
+# define material list
+material_list = [ um, lm,
+                 subplate1, subplate1_phase, subplate2, subplate3, subplate4, 
+                 backArc1, backArc2, 
+                 trans1, trans2, 
+                 craton1, craton2, 
+                 ribbon, 
+                 buoyStrip ]
---
+++ geo_model_properties.py
@@ -0,0 +1,14 @@
+# core UW bit
+from UWGeodynamics.scaling import units as u
+
+gravity = 9.8 * u.meter / u.second**2
+Tsurf   = 273.15 * u.degK
+Tint    = 1573.0 * u.degK
+
+kappa   = 1e-6   * u.meter**2 / u.second 
+
+boxLength = 6000.0 * u.kilometer
+boxHeight =  800.0 * u.kilometer
+boxWidth  = 3000.0 * u.kilometer
+
+
