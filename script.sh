e07b7f269183de4be299c9ca80f607e626ff0872
julian <house.of.jules@gmail.com>
2020-07-23 17:17:40 +1000

    Adding the needed ENV_VAR to the magnus script. It's sooooo ugly

--- script.sh
+++ script.sh
@@ -12,6 +12,6 @@ env
 # load Singularity
 module load singularity
 
-srun -N 2 -n 48 singularity exec \
+SINGULARITYENV_LD_LIBRARY_PATH=/opt/cray/pe/mpt/7.7.0/gni/mpich-gnu-abi/4.9/lib:/opt/cray/xpmem/2.2.15-6.0.7.1_5.16__g7549d06.ari/lib64:/opt/cray/ugni/6.0.14.0-6.0.7.1_3.18__gea11d3d.ari/lib64:/opt/cray/udreg/2.3.2-6.0.7.1_5.18__g5196236.ari/lib64:/opt/cray/pe/pmi/5.0.13/lib64:/opt/cray/alps/6.6.43-6.0.7.1_5.54__ga796da32.ari/lib64:/opt/cray/wlm_detect/1.3.3-6.0.7.1_5.8__g7109084.ari/lib64:/usr/lib64 srun -N 2 -n 48 singularity exec \
 /group/q97/ak5446p/images3/uwgeodynamics_v2.9.5.sif \
 bash -c "python3 geo_moresi_2014_subduction.py"
